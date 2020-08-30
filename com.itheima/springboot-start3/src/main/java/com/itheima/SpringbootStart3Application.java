6633bb643beaaaf3f3a2c265126cb54f3bb01ddd
xiaxuebo1991 <18363706182@163.com>
2020-08-30 22:39:16 +0800

    test

---
+++ com.itheima/springboot-start3/src/main/java/com/itheima/SpringbootStart3Application.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStart3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStart3Application.class, args);
+    }
+
+}
